b208f803d0501a2b3bed95f8d5302179cffa3c0e
reotrax <reo.trax127@gmail.com>
2017-08-08 17:36:55 +0900

    first commit.

--- jihanki/FirstViewController.swift
+++ jihanki/FirstViewController.swift
@@ -9,7 +9,11 @@
 import UIKit
 
 class FirstViewController: UIViewController {
-
+    
+    var drink1: String = "お茶"
+    var drink2: String = "オレンジ"
+    var drink3: String = "コーラ"
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
@@ -20,6 +24,20 @@ class FirstViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
+    @IBOutlet weak var output: UILabel!
+
+
+    @IBAction func drink1(_ sender: UIButton) {
+        output.text = drink1
+    }
+    
+    @IBAction func drink2(_ sender: UIButton) {
+        output.text = drink2
+    }
+    
+    @IBAction func drink3(_ sender: UIButton) {
+        output.text = drink2
+    }
 
 }
 
